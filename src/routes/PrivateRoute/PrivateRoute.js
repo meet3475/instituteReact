3588e1a8d765439731caff62c93b817db033e8d0
meet3475 <meetdobariya480@gmail.com>
2024-06-04 17:37:47 +0530

    done

--- src/routes/PrivateRoute/PrivateRoute.js
+++ src/routes/PrivateRoute/PrivateRoute.js
@@ -2,9 +2,7 @@ import React from 'react';
 import { Navigate, Outlet } from 'react-router-dom';
 
 function PrivateRoute(props) {
-
     const auth = true;
-
     return (
         auth ? <Outlet/> : <Navigate to={"/"} replace/>   
     );
